6f6d8ea6c38ddd7a32d426e803d1fd06d3250dda
danielnv051 <danielnv0910@gmail.com>
2024-01-30 21:39:51 +0330

    update readme

--- music.py
+++ music.py
@@ -6,9 +6,7 @@ song = (('c', 6),('d', 6),('e', 6),('e', 6),
         ('e', 6),('f', 6),('d', 6),('e', 6),('c', 3),
         ('d', 6),('e', 6),('d', 6),('e', 6),('c', 3),
         ('c', 6),('d', 6),('c', 6),('d', 6),('c', 3),
-        ('g', 6),('f', 6),('e', 6),('f', 6),('e', 3),
-        
-        )
+        ('g', 6),('f', 6),('e', 6),('f', 6),('e', 3))
 
 # Beats per minute (bpm) is really quarters per minute here
 psb.make_wav(song, fn = "song.wav", leg_stac = .7, bpm = 180)
