7cd610764137889d2edb15a2b37a08a1521570ea
danielnv051 <danielnv0910@gmail.com>
2024-01-30 20:52:55 +0330

    complete assignment 21

--- music.py
+++ music.py
@@ -1,12 +1,14 @@
 import pysynth_b as psb # a, b, e, and s variants available
 
-song = (
-  ('c', 4), ('c*', 4), ('eb', 4), 
-  ('g#', 4),  ('g*', 2), ('g5', 4),
-  ('g5*', 4), ('r', 4), ('e5', 16),
-  ('f5', 16),  ('e5', 16),  ('d5', 16),
-  ('e5*', 4) 
-)
+song = (('c', 6),('d', 6),('e', 6),('e', 6),
+        ('e', 6),('e', 6),('e', 6),('g', 6),('e', 3),
+        ('e', 6),('g', 6),('e', 6), ('g', 6),('e', 3),
+        ('e', 6),('f', 6),('d', 6),('e', 6),('c', 3),
+        ('d', 6),('e', 6),('d', 6),('e', 6),('c', 3),
+        ('c', 6),('d', 6),('c', 6),('d', 6),('c', 3),
+        ('g', 6),('f', 6),('e', 6),('f', 6),('e', 3),
+        
+        )
 
 # Beats per minute (bpm) is really quarters per minute here
-psb.make_wav(song, fn = "danube.wav", leg_stac = .7, bpm = 180)
+psb.make_wav(song, fn = "song.wav", leg_stac = .7, bpm = 180)
